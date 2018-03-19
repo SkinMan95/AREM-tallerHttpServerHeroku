6b3d4540e75f055be423048a79f00213b638e793
Alejandro Anzola <anzola95avila@gmail.com>
2018-03-18 20:58:32 -0500

    commit inicial

---
+++ src/main/java/edu/escuelaing/arem/taller/httpserverheroku/HttpServer.java
@@ -0,0 +1,61 @@
+package edu.escuelaing.arem.taller.httpserverheroku;
+
+import java.net.*;
+import java.io.*;
+
+/**
+ *
+ * @author Alejandro Anzola email: alejandro.anzola@mail.escuelaing.edu.co
+ */
+public class HttpServer {
+
+    public static final int DEFAULT_PORT = 36000;
+    
+    public static void main(String[] args) throws IOException {
+        int port = new Integer(System.getenv("PORT"));
+        
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: " + port + ".");
+            System.exit(1);
+        }
+
+        Socket clientSocket = null;
+        try {
+            System.out.println("Listo para recibir ...");
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.err.println("Accept failed.");
+            System.exit(1);
+        }
+        PrintWriter out = new PrintWriter(
+                clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        String inputLine, outputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println("Recibí: " + inputLine);
+            if (!in.ready()) {
+                break;
+            }
+        }
+        outputLine
+                = "<!DOCTYPE html>"
+                + "<html>"
+                + "<head>"
+                + "<meta charset=\"UTF-8\">"
+                + "<title>Title of the document</title>\n"
+                + "</head>"
+                + "<body>"
+                + "<h1>Mi propio mensaje</h1>"
+                + "</body>"
+                + "</html>" + inputLine;
+        out.println(outputLine);
+        out.close();
+        in.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+}
